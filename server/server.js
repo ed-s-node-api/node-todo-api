b3d116106ecc433432777e7d1b890acd1b1fd232
Ed McCormic <edmccormic@gmail.com>
2019-03-01 14:24:25 -0700

    Added get by Id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 app.use(bodyParser.json());
@@ -32,6 +33,14 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+})
 
 module.exports = {app};
 
